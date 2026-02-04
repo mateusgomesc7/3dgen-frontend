5987bc000daf280d507d4f65b31d32b782da49ec
Mateus Gomes <mateusgomes.dev@gmail.com>
2026-02-03 20:08:53 -0400

    feat: implement error handling in API responses with snackbar notifications

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,3 +1,5 @@
+import { useSnackbarStore } from "@/modules/snackbar/snakcbar.store";
+
 export const api = $fetch.create({
   baseURL: useRuntimeConfig().public.apiBaseUrl,
   credentials: "include",
@@ -6,5 +8,25 @@ export const api = $fetch.create({
     options.headers = {
       ...options.headers,
     };
+  },
+
+  async onResponseError({ response }) {
+    const snackbarStore = useSnackbarStore();
+
+    const errorMessage =
+      response._data?.error.detail || "An unexpected error occurred.";
+    const errorStatus = response.status;
+
+    switch (errorStatus) {
+      case 500:
+        snackbarStore.showSnackbar(
+          "Server error. Please try again later.",
+          "error",
+        );
+        break;
+      default:
+        snackbarStore.showSnackbar(errorMessage, "error");
+        break;
+    }
   },
 });
